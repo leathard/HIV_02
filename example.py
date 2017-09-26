44711fae13c251b055a1425a6788b2d4ce4996f8
leathard <jkmezic@american.edu>
2017-09-26 15:57:55 -0400

    Add files via upload

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
-from isaac import concurrency
+from isaac import concurrency # isaac/concurrency.py
 
-class PersonExample(concurrency.Person):
+
+class PersonExample01(concurrency.Person):
     def __init__(self,
         sex,
         comsex,    #commercial sex worker?
@@ -19,6 +20,19 @@ class PersonExample(concurrency.Person):
             cls=self.__class__,
             comsex="not" if self.sex=="M" else ""
             ))
+
+    @property
+    def ART(self):
+        return 1 == self.treatment
+
+    @property
+    def PrEP(self):
+        return 2 == self.treatment
+
+    def is_available(self, yearDay=None):
+        #Probably better to just pass the day and compute the yearDay here ...
+        return yearDay >= self.activeRange[0] and yearDay < self.activeRange[-1]
+
 
 if __name__=="__main__":
     p = PersonExample('M', True, 0, None, dict(prng=None))
